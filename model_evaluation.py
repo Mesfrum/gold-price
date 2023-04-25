7d8f6c3eee553809e67314db29a9ae79a0a9a13b
Akhil <pletiakhil100@gmail.com>
2023-04-26 02:35:48 +0530

    Add webpage

--- model_evaluation.py
+++ model_evaluation.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as plticker
 import seaborn as sns
 import pandas as pd
+import numpy as np
 import os.path
 from train_model import check_folder_exists
 
@@ -17,65 +18,71 @@ def check_pickle_exists():
         from train_model import main
         main()
 
-
 def import_pickle_files():
-    global regressor
-    global X_test
-    global Y_test
     regressor = joblib.load("pickle_files/regressor.pkl")
     X_test = joblib.load("pickle_files/X_test.pkl")
     Y_test = joblib.load("pickle_files/Y_test.pkl")
+    return regressor,X_test,Y_test
 
+def master():
+    check_folder_exists("pickle_files")
+    check_folder_exists("media_plots")
 
-check_folder_exists("pickle_files")
-check_folder_exists("media_plots")
+    # Load the model from the file
+    try:
+        regressor,X_test,Y_test = import_pickle_files()
+    except FileNotFoundError:
+        check_pickle_exists()
+        regressor,X_test,Y_test = import_pickle_files()
 
-# Load the model from the file
-try:
-    import_pickle_files()
-except FileNotFoundError:
-    check_pickle_exists()
-    import_pickle_files()
+    # Use the loaded model to make predictions and test
+    test_data_prediciton = regressor.predict(X_test)
+    
+    #  load test data as dataframe to pcikle for later use
+    pd.DataFrame(test_data_prediciton).to_pickle("pickle_files/test_data_prediciton.pkl")
+    
+    print("datatype of prediction made - ", type(test_data_prediciton), end="\n\n")
+    print(
+        "actual prediction made using non-gold values (first five values for simplicity) -"
+    )
+    print(test_data_prediciton[0:5], end="\n\n")
 
-# Use the loaded model to make predictions and test
-test_data_prediciton = regressor.predict(X_test)
-print("datatype of prediction made - ", type(test_data_prediciton), end="\n\n")
-print(
-    "actual prediction made using non-gold values (first five values for simplicity) -"
-)
-print(test_data_prediciton[0:5], end="\n\n")
+    # R squared error to computer error rate
+    error_score = metrics.r2_score(Y_test, test_data_prediciton)
+    print("ACCURACY - ", (error_score * 100), "%", sep="")
 
-# R squared error to computer error rate
-error_score = metrics.r2_score(Y_test, test_data_prediciton)
-print("ACCURACY - ", (error_score * 100), "%", sep="")
+    # visualize error rate
+    Y_test = list(Y_test) 
+    test_data_prediciton = list(test_data_prediciton)
+    
+    # fix below repition
+    plt.plot(Y_test, color="blue", label="Actual Value")
+    plt.title("Number of values")
+    plt.ylabel("GOLD prices")
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig("media_plots/actual_values.png", bbox_inches="tight")
+    plt.close()
+    # plt.show()
 
-# visualize error rate
-Y_test = list(Y_test)
-test_data_prediciton = list(test_data_prediciton)
+    plt.plot(test_data_prediciton, color="red", label="predicted Value")
+    plt.title("Number of values")
+    plt.ylabel("GOLD prices")
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig("media_plots/predicted_values.png", bbox_inches="tight")
+    plt.close()
+    # plt.show()
 
-# fix below repition
-
-plt.plot(test_data_prediciton, color="red", label="predicted Value")
-plt.title("Number of values")
-plt.ylabel("GOLD prices")
-plt.legend()
-plt.tight_layout()
-plt.savefig("media_plots/predicted_values.png", bbox_inches="tight")
-# plt.show()
-
-plt.plot(Y_test, color="blue", label="Actual Value")
-plt.title("Number of values")
-plt.ylabel("GOLD prices")
-plt.legend()
-plt.tight_layout()
-plt.savefig("media_plots/actual_values.png", bbox_inches="tight")
-# plt.show()
-
-plt.plot(test_data_prediciton, color="red", label="predicted Value")
-plt.plot(Y_test, color="blue", label="Actual Value")
-plt.title("Number of values")
-plt.ylabel("GOLD prices")
-plt.legend()
-plt.tight_layout()
-plt.savefig("media_plots/price_comparison.png", bbox_inches="tight")
-# plt.show()
+    plt.plot(test_data_prediciton, color="red", label="predicted Value",alpha = 0.5)
+    plt.plot(Y_test, color="blue", label="Actual Value",alpha = 0.5)
+    plt.title("Number of values")
+    plt.ylabel("GOLD prices")
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig("media_plots/price_comparison.png", bbox_inches="tight")
+    plt.close()
+    # plt.show()
+    
+if __name__ == '__main__':
+    master()
